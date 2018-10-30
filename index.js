2ce758f77946d7a0fd5f4a8cd766b0c9d5d75036
Temicka Brown <temicka@Temickas-MacBook-Air.local>
2018-10-29 23:19:04 -0400

    working on crud application

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.listen(4000, () => {
+
+app.get("/:name", (req, res) => {
+  res.send(`hello ${req.params.name}`)
+})
+
+
+app.listen(4000, () => {//listening on port 4000
     console.log("port 4000 BABY! YEA!")
   })
