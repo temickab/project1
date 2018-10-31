1841d38c727401d194e01e62e07610a9e7fe7bf2
Temicka Brown <temicka@Temickas-MacBook-Air.local>
2018-10-30 23:38:07 -0400

    added body parser

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true})) //body-parser
+
 app.listen(3000, () => {
     console.log('Yup, its on 3000')
   })
 
 
-//   app.get('/', function(req, res) {
-//     res.send('Hello World')
-//   })
-
-//   app.get("/", (request, response) => {
-//     response.send("Hello World")
-//   })
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
   })
 
-//   res.sendFile(__dirname + '/index.html')
+  app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })
